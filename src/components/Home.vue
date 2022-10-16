fb0a57c630653d7911887c06a2ee091402351bbd
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2022-10-16 15:34:39 +0100

    Corrected spelling mistake

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<section id="intro">
-			<p>Quotes have the power to motivate, encourage, and inspire. They can provide us with wisdom and insight into the true nature of what we're experiencing in life. And perhaps most importantly they can light the way in times of diffculty. To find a quote that resonates with your circumstances or how your're feeling, choose a keyword from the list below:</p>
+			<p>Quotes have the power to motivate, encourage, and inspire. They can provide us with wisdom and insight into the true nature of what we're experiencing in life. And perhaps most importantly they can light the way in times of diffculty. To find a quote that resonates with your circumstances or how you're feeling, choose a keyword from the list below:</p>
 		</section>
 		<section id="keywords">
 			<router-link v-for="(keyword, index) in keywords" :key="index" :to="{ name: 'quote', params: { keyword: keyword } }" class="keyword">
